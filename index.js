39b374284fb79e4cb0d3a6acf31ce64d9d57748e
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-06-25 22:59:15 -0300

    Lógica JS para retornar a idade concluída

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('ola')
+const submit = document.querySelector('button')
+const inputDay = document.querySelector('.inputDay')
+const inputMonth = document.querySelector('.inputMonth')
+const inputYear = document.querySelector('.inputYear')
+const errors = document.querySelectorAll('.error')
+
+const years = document.querySelector('.years')
+const months = document.querySelector('.months')
+const days = document.querySelector('.days')
+
+submit.addEventListener('click', () => {
+  const currentDate = new Date()
+  const dataDeNascimento = new Date(inputYear.value, inputMonth.value - 1, inputDay.value)
+
+  const yearsText = Math.floor(currentDate.getFullYear() - dataDeNascimento.getFullYear())
+  const monthsText = Math.floor(currentDate.getMonth() - dataDeNascimento.getMonth())
+  const daysText = currentDate.getDate() - inputDay.value
+
+  years.innerHTML = yearsText
+  months.innerHTML = monthsText < 0 ? monthsText + 12 : monthsText
+  days.innerHTML = Math.abs(daysText)
+})
