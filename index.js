fd23cd67042606da933352f3652a428289e46936
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-06-26 22:00:17 -0300

    Corrigindo a lógica de calcular a idade

--- index.js
+++ index.js
@@ -10,13 +10,42 @@ const days = document.querySelector('.days')
 
 submit.addEventListener('click', () => {
   const currentDate = new Date()
-  const dataDeNascimento = new Date(inputYear.value, inputMonth.value - 1, inputDay.value)
+  const dateOfBirth = new Date(inputYear.value, inputMonth.value - 1, inputDay.value)
 
-  const yearsText = Math.floor(currentDate.getFullYear() - dataDeNascimento.getFullYear())
-  const monthsText = Math.floor(currentDate.getMonth() - dataDeNascimento.getMonth())
-  const daysText = currentDate.getDate() - inputDay.value
+  let accYear = 0
+  let accMonth = 0
 
-  years.innerHTML = yearsText
-  months.innerHTML = monthsText < 0 ? monthsText + 12 : monthsText
-  days.innerHTML = Math.abs(daysText)
+  const Feb = currentDate.getFullYear() % 400 === 0 ? 29 : currentDate.getFullYear() % 100 === 0 ? 28 : currentDate.getFullYear() % 4 === 0 ? 29 : 28
+  const monthsYears = [ 31, Feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  const yearsText = () => {
+    return currentDate.getMonth() + 1 > Number(inputMonth.value) ? currentDate.getFullYear() - Number(inputYear.value) : 
+    (currentDate.getFullYear() - Number(inputYear.value)) - 1
+  }
+  const monthsText = () => {
+    return currentDate.getMonth() - dateOfBirth.getMonth() < 0 ? (currentDate.getMonth() - dateOfBirth.getMonth()) + 12 : currentDate.getMonth() - dateOfBirth.getMonth()
+  }
+  const daysText = () => {
+    if(monthsText() == 0 && currentDate.getDate() < Number(inputDay.value)) {
+      accMonth = 11
+      return (monthsYears[currentDate.getMonth() - 1] - Number(inputDay.value)) + currentDate.getDate()
+    } else if(monthsText() == 0 && currentDate.getDate() == Number(inputDay.value)) {
+      accYear = 1
+      return 0
+    } else {
+      accYear = 1
+      return currentDate.getDate() - Number(inputDay.value)
+    }
+  }
+  // const daysText = currentDate.getDate() - inputDay.value
+
+  days.innerHTML = daysText()
+  months.innerHTML = monthsText() + accMonth
+  years.innerHTML = yearsText() + accYear
+  // if(monthsText === 0 || currentDate.getDate() < Number(inputDay.value)) {
+  //   let day = Math.abs((monthsYears[currentDate.getMonth()] - currentDate.getDate()) + Number(inputDay.value))
+  //   days.innerHTML = day
+  // } else {
+  //   days.innerHTML = Math.abs(daysText)
+  // }
 })
